2eb426cab466445c98513fa3140d077e1e5b4a2e
Riccardo Moschetti <riccardo.moschetti@avanade.com>
2022-05-26 19:04:34 +0200

    improvements in AADconfiguration

--- src/WindowsDesktopClient/AADConfigurations.cs
+++ src/WindowsDesktopClient/AADConfigurations.cs
@@ -7,8 +7,7 @@ namespace WindowsDesktopClient
         Null, BankInfoAPI, MSGraph, AzureARM
     }
     public struct AADTokenResponse
-    {
-        public string? token_type { get; set; }
+    {    public string? token_type { get; set; }
         public string? scope { get; set; }
         public int? expires_in { get; set; }
         public int? ext_expires_in { get; set; }
